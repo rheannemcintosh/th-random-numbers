2142dee4bf79a82e7401015d20ee5da7107bc39b
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-04-25 19:38:54 +0100

    Display a random number between 1 and the provided number

--- js/random.js
+++ js/random.js
@@ -1,7 +1,11 @@
 // Collect input from a user
+const input = prompt("Please enter a random number");
 
 // Convert the input to a number
+const upper = parseInt(input);
 
 // Use Math.random() and the user's number to generate a random number
+const random = Math.floor(Math.random() * upper) + 1;
 
 // Create a message displaying the random number
+console.log(`${random} is a random number between 1 and ${upper}.`);
